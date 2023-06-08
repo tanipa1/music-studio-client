7cf70fd410cd111bfd6f5d267fef90f8c0486419
tanipa1 <tania35.291@gmail.com>
2023-06-08 13:22:40 +0600

    Login Design

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home/Home";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Login from "../Pages/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -15,7 +16,11 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
-        }
+        },
+        {
+            path: '/login',
+            element: <Login></Login>
+        },
       ]
     },
   ]);
